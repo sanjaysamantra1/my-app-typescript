4103559f97019b0fa6a82e403ac6bd526d062778
Sanjay Samantra <sanjaysamantra1@gmail.com>
2023-06-16 07:52:59 +0530

    created a react project using typescript

---
+++ src/components/body/Body.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function Body() {
+  return <div style={{minHeight:"400px"}}>This is body Component</div>
+}
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function Footer() {
+  return <h1>This is My Footer</h1>
+}
